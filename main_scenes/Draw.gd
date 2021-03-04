4a627a4434c4790cc52534b09b3265f1efef8042
Xpost2000 <xpost2000cod@gmail.com>
2021-03-04 17:56:41 -0500

    okay actually fixed

--- main_scenes/Draw.gd
+++ main_scenes/Draw.gd
@@ -36,14 +36,14 @@ func _draw():
 			if world.in_bounds(offset_position):
 				for y in range(world.CHUNK_MAX_SIZE):
 					for x in range(world.CHUNK_MAX_SIZE):
-						var tile_position = Vector2(x + (chunk_x * world.CHUNK_MAX_SIZE), (1+y) + (chunk_y * world.CHUNK_MAX_SIZE));
+						var tile_position = Vector2(x + (chunk_x * world.CHUNK_MAX_SIZE), (y) + (chunk_y * world.CHUNK_MAX_SIZE));
 						var cell_id;
 						var cell_symbol = "#" if world.is_solid_tile(tile_position) else ".";
 						var light_value = world.is_cell_visible(tile_position);
 						var cell_color = Color(1, 1, 1) if world.is_solid_tile(tile_position) else Color(0, 1, 0);
 
 						if world.is_cell_visible(tile_position):
-							draw_string(game_font, tile_position * Vector2((FONT_HEIGHT/2), FONT_HEIGHT), cell_symbol, Color(cell_color * light_value));
+							draw_string(game_font, (tile_position+Vector2(0, 1)) * Vector2((FONT_HEIGHT/2), FONT_HEIGHT), cell_symbol, Color(cell_color * light_value));
 	if entities:
 		# stupid boldness.
 		for entity in entities.entities:
