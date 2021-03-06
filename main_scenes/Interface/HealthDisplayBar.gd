690bdcc18ad60ad229108e8e961158adc99571a1
Xpost2000 <xpost2000cod@gmail.com>
2021-03-06 16:04:32 -0500

    status effect text

--- main_scenes/Interface/HealthDisplayBar.gd
+++ main_scenes/Interface/HealthDisplayBar.gd
@@ -1,7 +1,8 @@
 extends ColorRect
 
 onready var health_bar = $Layout/Health;
-onready var name_label = $Layout/Name;
+onready var name_label = $Layout/Layout/Name;
+onready var status_effect_layout = $StatusEffectLayout;
 
 const HEALTH_HEALTHY = Color(64/255.0, 120/255.0, 32/255.0, 255/255.0);
 const HEALTH_MEDIUM = Color(204/255.0, 182/255.0, 35/255.0, 255/255.0);
@@ -14,6 +15,21 @@ func update_from(entity):
 
 	var health_percentage = entity.health_percentage();
 	var foreground_style = health_bar.get("custom_styles/fg");
+
+	for child in status_effect_layout.get_children():
+		status_effect_layout.remove_child(child);
+
+	if entity.adrenaline_active_timer > 0:
+		var adrenaline_status_label = Label.new();
+		adrenaline_status_label.text = "(ADR)";
+		adrenaline_status_label.add_color_override("font_color", Color.yellow);
+		status_effect_layout.add_child(adrenaline_status_label);
+
+	if entity.current_medkit and entity.use_medkit_timer > 0:
+		var use_medkit_label = Label.new();
+		use_medkit_label.text = "(HEALING)";
+		use_medkit_label.add_color_override("font_color", Color.green);
+		status_effect_layout.add_child(use_medkit_label);
 
 	if health_percentage >= 0.6:
 		foreground_style.bg_color = (HEALTH_HEALTHY);
